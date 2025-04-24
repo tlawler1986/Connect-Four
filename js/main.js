34d41cf25818ac18ecee957a37385fdccf2294d8
Tim Lawler <niftytim98@hotmail.com>
2025-04-24 13:02:32 -0400

    Connect-Four

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 
 
 /*----- state variables -----*/
-
+// Define, but do not assign to (initialize)
+let board;  // 2d array / 1/-1 -> player value; null -> cell empty
+let winner; // null -> no winner or tie, game is in progress; 1/-1 -> the player that won; 'Tie' -> the game has tied
+let turn;  // 1/-1 -> player whose turn it is
 
 /*----- cached elements  -----*/
 
@@ -11,3 +14,13 @@
 
 
 /*----- functions -----*/
+
+
+// The init function's purpose is to initalize all state, then call render()
+function init() {
+
+    winner = null;
+    turn = 1;
+    // render();
+}
+
